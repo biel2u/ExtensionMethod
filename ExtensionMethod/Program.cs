27f7c6bf74f505bbecee0d0be71a5cbf5e11b81d
Patryk Bielecki <p.patrykbielecki@gmail.com>
2017-12-27 12:10:00 +0100

    extension method implemented

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Type word to reverse...");
+            var toReverse = Console.ReadLine();
+
+            var reversed = toReverse.StringReverse();
+
+            Console.WriteLine("Reversed word {0} is: {1}", toReverse, reversed);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExtensionMethod/ReverseExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+    public static class ReverseExtension
+    {
+        public static string StringReverse(this string word)
+        {
+            var toArray = word.ToCharArray();
+            Array.Reverse(toArray);
+            return new string(toArray);
+        }
+    }
+}
